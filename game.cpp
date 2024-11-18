9db6f27356bb0038ee2929fbe3c9ee7cb5785e43
Ludde <luddenorin@gmail.com>
2024-11-18 16:45:53 +0100

    feat: Game::checkWin completed and tested

--- game.cpp
+++ game.cpp
@@ -3,7 +3,7 @@
 
 
 void Game::displayBoard(){
-    for(auto& row : this->board){
+    for(auto& row : this->board) {
         for(auto&cell : row){
             std::cout << cell;
         }
@@ -11,3 +11,31 @@ void Game::displayBoard(){
         std::cout << "\n";    
     }
  }
+
+bool Game::checkWin(){
+    for (auto i = 0; i< this->board.size();i++) {
+        if ((board[i][0] != " " &&
+            board[i][0] == board[i][1] &&
+            board[i][1] == board[i][2])||
+           (board[0][i] != " "  &&
+            board[0][i] == board[1][i] &&
+            board[1][i] == board[2][i])){
+                return true;
+        }   
+    }
+
+    if(board[0][0] != " " &&
+       board[0][0] == board[1][1] &&
+       board[1][1] == board[2][2]) {
+        return true;
+
+    }
+    
+    if (board[0][2] != " " &&
+        board[0][2] == board[1][1] &&
+        board[1][1] == board[2][0]) {
+        return true;
+    }
+
+    return false;
+}
--- game.hpp
+++ game.hpp
@@ -4,12 +4,16 @@
 class Game{
     private:
     public: 
+
+    // check 8 olika kombinationer
     std::vector <std::vector <std::string>>board {{" "," "," "},
                                                   {" "," "," "},
                                                   {" "," "," "}};
 
 
+    //check
     void displayBoard();
+
     bool checkWin();
 
     bool checkDraw();
