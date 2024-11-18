0e88635af958dbbfd5f03747616a6d2e33cb9139
Ludde <luddenorin@gmail.com>
2024-11-18 15:23:24 +0100

    chore cleanup

--- game.hpp
+++ game.hpp
@@ -4,12 +4,15 @@
 class Game{
     private:
     public: 
+
+    // check
     std::vector <std::vector <std::string>>board {{" "," "," "},
                                                   {" "," "," "},
                                                   {" "," "," "}};
 
 
     void displayBoard();
+
     bool checkWin();
 
     bool checkDraw();
